2b1b195a39d2eaee2b0524b08c94acbe0d2f0dff
emma <enabojohn@gmail.com>
2021-03-23 11:29:36 +0300

    Added get user infor, all user infor for permitted user, and logout routes to the userRoute file

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,27 +1,28 @@
 const router = require('express').Router();
 const userCtrl = require('../controllers/userCtrl');
-
+const auth = require('../middleware/auth');
+const authAdmin = require('../middleware/authAdmin');
 
 
 router.post('/register', userCtrl.register);
+
 router.post('/activation', userCtrl.activateEmail);
+
 router.post('/login', userCtrl.login);
+
 router.post('/refresh_token', userCtrl.getAccessToken);
 
+router.post('/forgot', userCtrl.forgotPassword);
 
+router.post('/reset',auth,  userCtrl.resetPassword);
 
+router.get('/infor',auth,  userCtrl.getUserInfor);
 
+router.get('/all_infor', auth, authAdmin, userCtrl.getUsersAllInfor);
 
+router.get('/logout',  userCtrl.logout);
 
-
-
-
-
-
-
-
-
-module.exports = router
+module.exports = router;
 
 
 
