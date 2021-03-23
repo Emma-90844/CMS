cb1be5e6ea0710c2796620b4cd79638678489bac
emma <enabojohn@gmail.com>
2021-03-23 11:31:28 +0300

    Implemented logout and clearing cookies on logout -clearing the refresh token

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -142,9 +142,29 @@ const userCtrl = {
             res.json(user);
 
         } catch (err) {
+            return res.status(500).json({msg: err.message});
+        }
+    },
+    getUsersAllInfor: async (req, res) => {
+        try {
+            const users = await Users.find().select('-password')
+            res.json(users);
+        } catch (err) {
+            return res.status(500).json({msg: err.message});
+        }
+    },
+    logout: async (req, res) => {
+        try {
+            res.clearCookie('refreshtoken',  {
+                path:'/user/refresh_token'
+               
+            })
+            return res.json({msg: "Logged out"})
+        } catch (err) {
             return res.status(500).json({msg: err.message})
         }
-    }
+    },
+    update
 
 }
 
